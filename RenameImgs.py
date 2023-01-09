be8a03e6b12eae2b8fb0e27ffd0968643facc448
Hakii <89122368+XuejiFang@users.noreply.github.com>
2023-01-09 23:17:49 +0800

    Delete RenameImgs.py

--- RenameImgs.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path  = '../face_recognition_system/database/Me/'
-name = 'Hakim'
-for i, name in enumerate(os.listdir(path)):
-    print(name)
-
-    newName = name + str(i) + '.png'
-    os.rename(path+name, path+newName)
